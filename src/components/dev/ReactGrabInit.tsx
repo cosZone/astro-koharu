46db549205e135f2f7607ef56c46d70c0801e45e
yusixian <i@cosine.ren>
2026-05-16 22:24:57 +0800

    refactor(ReactGrab): update ReactGrabInit component and adjust cache TTL handling

--- src/components/dev/ReactGrabInit.tsx
+++ src/components/dev/ReactGrabInit.tsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 
-// react-grab: dev-only tool for copying React component context (file, component name, HTML)
-// This component is only rendered when REACT_GRAB=true is set in .env
 export default function ReactGrabInit() {
   useEffect(() => {
     import('react-grab');
